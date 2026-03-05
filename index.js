f04bdeec2da625a70f07bceac451768c15fc9932
Mahadi176 <mahadi210176@gmail.com>
2026-03-05 17:34:34 +0600

    oushy

--- index.js
+++ index.js
@@ -15,14 +15,24 @@ const displayLevelWords = (words) => {
     const wordContainer = document.getElementById('word-container')
     wordContainer.innerHTML = "";
 
+    if(words.length == 0){
+         wordContainer.innerHTML = `
+         <div class="text-center col-span-full py-10 space-y-10">
+          <img class="mx-auto" src="assets/alert-error.png" alt="">
+            <p class="text-xl font-medium text-gray-700">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-4xl font-bold">নেক্সট Lesson এ যান</h2>
+       </div>
+         `;
+    }
+
     words.forEach((word) => {
         console.log(word)
         const card = document.createElement('div')
         card.innerHTML = `
-         <div class="bg-white rounder-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
-            <p class="font-semibold">${word.meaning}</p>
-            <div class="text-2xl font-medium font-bangla">${word.pronunciation}</div>
+         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word ? word.word : 'not available'}</h2>
+            <p class="font-semibold">${word.meaning? word.meaning : 'not available'  }</p>
+            <div class="text-2xl font-medium font-bangla">${word.pronunciation? word.pronunciation : 'not available'  }</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-gray-300"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-gray-300"><i class="fa-solid fa-volume-high"></i></button>
