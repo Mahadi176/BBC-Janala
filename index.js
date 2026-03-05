6975f5550f62333db906b7301441faac8d731d2e
Mahadi176 <mahadi210176@gmail.com>
2026-03-05 17:10:51 +0600

    push

--- index.js
+++ index.js
@@ -12,7 +12,25 @@ const loadLevelWord = (id)  => {
 } 
 
 const displayLevelWords = (words) => {
-    
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = "";
+
+    words.forEach((word) => {
+        console.log(word)
+        const card = document.createElement('div')
+        card.innerHTML = `
+         <div class="bg-white rounder-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold">${word.meaning}</p>
+            <div class="text-2xl font-medium font-bangla">${word.pronunciation}</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-gray-300"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-gray-300"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `
+        wordContainer.append(card)
+    })
 }
 
 const displayLessons = (lessons) => {
